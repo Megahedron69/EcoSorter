b41051e132260d0686b08056e111cfba91007d16
Megahedron69 <karticjoshi68@gmail.com>
2023-11-20 22:06:54 +0530

    bug fixes

--- metro.config.js
+++ metro.config.js
@@ -6,6 +6,8 @@ module.exports = getDefaultConfig(__dirname);
 module.exports = (() => {
   const defaultConfig = getDefaultConfig(__dirname);
   const { assetExts } = defaultConfig.resolver;
+  defaultConfig.resolver.assetExts.push("hcscript");
+  defaultConfig.transformer.assetPlugins = ["expo-asset/tools/hashAssetFiles"];
   return {
     resolver: {
       // Add .bin to assetExts.
